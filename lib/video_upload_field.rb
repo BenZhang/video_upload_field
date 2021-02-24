3bed76b38520dd2b1eb4aaab7085dbf669c2e4bf
ian wang <ian.wang@pixelforcesystems.com.au>
2021-02-24 11:50:52 +1030

    create gem

---
+++ lib/video_upload_field.rb
@@ -0,0 +1,6 @@
+require "video_upload_field/version"
+
+module VideoUploadField
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/video_upload_field/version.rb
@@ -0,0 +1,3 @@
+module VideoUploadField
+  VERSION = "0.1.0"
+end
