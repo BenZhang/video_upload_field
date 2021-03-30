a920a7f273d3e7e9358e29b9eb690599ae2201d1
Jianqiu Li <jianqiuli@pixelforcesystems.com.au>
2021-03-30 16:24:38 +1030

    tweak

--- lib/generators/upload_assets_generator/install/install_generator.rb
+++ lib/generators/upload_assets_generator/install/install_generator.rb
@@ -5,9 +5,9 @@ module UploadAssetsGenerator
 
       desc "This generator installs the javascript needed for uploads"
       def copy_the_files
-        copy_file "javascript/controllers/direct_uploader_controller.js", "app/javascript/controllers/direct_uploader_controller.js"
         copy_file "javascript/packs/custom_uploader.js", "app/javascript/packs/custom_uploader.js"
-        copy_file "javascript/packs/direct_uploads.js", "app/javascript/packs/direct_uploads.js"
+        copy_file "javascript/packs/direct_uploads_listener.js", "app/javascript/packs/direct_uploads_listener.js"
+        copy_file "javascript/packs/instant_upload_initializer.js", "app/javascript/packs/instant_upload_initializer.js"
         copy_file "assets/stylesheets/components/direct_uploads.sass", "app/assets/stylesheets/components/direct_uploads.sass"
       end
     end
--- lib/video_upload_field/inputs/file_upload_input.rb
+++ lib/video_upload_field/inputs/file_upload_input.rb
@@ -5,10 +5,14 @@ module VideoUploadField
     class FileUploadInput < SimpleForm::Inputs::Base
       def input(wrapper_options)
         if options[:direct_upload].present?
-          direct_upload_options = merge_wrapper_options({ direct_upload: true }, { data: { action: 'change->direct-uploader#direct_upload' } })
+          direct_upload_options = merge_wrapper_options({ direct_upload: true }, { data: { target: 'upload-input' } })
         else
-          direct_upload_options = {}
+          direct_upload_options = { data: { target: 'upload-input' } }
         end
+
+        cover_width = options[:cover_width] || 300
+        cover_height = options[:cover_height] || 300
+        display_size = "#{cover_width}x#{cover_height}>"
 
         merged_input_options = merge_wrapper_options(input_html_options, wrapper_options)
         merged_input_options = merge_wrapper_options(merged_input_options, direct_upload_options)
@@ -20,9 +24,9 @@ module VideoUploadField
                            else
                              if object.try(reflection_or_attribute_name.to_sym).try(:attached?) && object.try(reflection_or_attribute_name.to_sym).blob.content_type != 'application/pdf'
                                if object.try(reflection_or_attribute_name.to_sym).previewable?
-                                 rails_representation_url(object.try(reflection_or_attribute_name.to_sym).preview(resize_to_limit: [500, 300]).processed.image.variant(resize: '500x300>'), only_path: true)
+                                 rails_representation_url(object.try(reflection_or_attribute_name.to_sym).preview(resize_to_limit: [cover_width, cover_height]).processed.image.variant(resize: display_size), only_path: true)
                                else
-                                 rails_representation_url(object.try(reflection_or_attribute_name.to_sym).variant(resize: '500x300>'), only_path: true)
+                                 rails_representation_url(object.try(reflection_or_attribute_name.to_sym).variant(resize: display_size), only_path: true)
                                end
                              else
                                ""
@@ -30,10 +34,10 @@ module VideoUploadField
                            end
 
         "<div class='upload-wrapper'>
-          <div class='uploaded-container' style='background-image: url(#{object_image_url})'></div>
-          <div class='upload-container' data-controller='direct-uploader'>
+          <div class='upload-cover-container' style='background-image: url(#{object_image_url});'></div>
+          <div class='upload-input-container'>
             <label>
-              <div class='upload-text'>#{upload_text}</div>
+              <div class='upload-text button'>#{upload_text}</div>
               <div class='upload-icon'></div>
               <div class='hide'>#{@builder.file_field(attribute_name, merged_input_options)}</div>
             </label>
